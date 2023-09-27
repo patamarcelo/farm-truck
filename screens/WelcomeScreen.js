54b15d30afad9cdb487aabe27a8dd9f5917e0dd7
patamarcelo <patamarcelo@gmail.com>
2023-09-27 10:35:44 -0300

    resumo

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -4,6 +4,7 @@ import CardRomaneio from "../components/romaneio/CardTruck";
 import { ScrollView } from "react-native-gesture-handler";
 import data from "../utils/dummy-data";
 import { Colors } from "../constants/styles";
+import { Divider } from "react-native-elements";
 
 function WelcomeScreen() {
 	const renderRomaneioList = (itemData) => {
@@ -12,13 +13,18 @@ function WelcomeScreen() {
 
 	return (
 		<View style={styles.rootContainer}>
-			<FlatList
-				data={data}
-				keyExtractor={(item) => item.id}
-				renderItem={renderRomaneioList}
-			/>
-			{/* <Text style={styles.title}>Welcome!</Text>
+			<View style={styles.resumoContainer}>
+				<Text>RESUMO CONTAINER</Text>
+			</View>
+			<View style={styles.listContainer}>
+				<FlatList
+					data={data}
+					keyExtractor={(item) => item.id}
+					renderItem={renderRomaneioList}
+				/>
+				{/* <Text style={styles.title}>Welcome!</Text>
 			<Text>You authenticated successfully!</Text> */}
+			</View>
 		</View>
 	);
 }
@@ -32,6 +38,14 @@ const styles = StyleSheet.create({
 		alignItems: "center",
 		paddingTop: 32,
 		backgroundColor: "whitesmoke"
+	},
+	resumoContainer: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center"
+	},
+	listContainer: {
+		flex: 3
 	},
 	title: {
 		fontSize: 20,
