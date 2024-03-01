44baedaca0b4d3ffb701c2e34421f84dcc2d743f
patamarcelo <patamarcelo@gmail.com>
2024-03-01 16:11:49 -0300

    adjust to upload playStore

--- components/Auth/AuthContent.js
+++ components/Auth/AuthContent.js
@@ -30,7 +30,7 @@ function AuthContent({ isLogin, onAuthenticate }) {
 		password = password.trim();
 
 		const emailIsValid = email.includes("@");
-		const passwordIsValid = password.length > 6;
+		const passwordIsValid = password.length > 5;
 		const emailsAreEqual = email === confirmEmail;
 		const passwordsAreEqual = password === confirmPassword;
 		if (isLogin) {
