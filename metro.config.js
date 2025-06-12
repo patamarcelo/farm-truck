ecf5762b61ec80d736d1e65ac29a008fbd5a768b
patamarcelo <patamarcelo@gmail.com>
2025-06-12 10:07:10 -0300

    updated version to sdk 53

---
+++ metro.config.js
@@ -0,0 +1,14 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+defaultConfig.resolver.sourceExts.push('cjs');
+
+// This is the new line you should add in, after the previous lines
+defaultConfig.resolver.unstable_enablePackageExports = false;
+
+module.exports = defaultConfig;
